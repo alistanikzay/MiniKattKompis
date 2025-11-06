181e859ecafbdd4fa514b75afe196b2d17c4894a
alistanikzay <ali.kawsar.stanikzay@iths.se>
2025-11-07 00:06:27 +0100

    CatGraphQLController

---
+++ src/main/java/org/example/minikattkompis/controller/CatGraphQLController.java
@@ -0,0 +1,23 @@
+package org.example.minikattkompis.controller;
+
+import org.example.minikattkompis.model.Cat;
+import org.example.minikattkompis.service.CatService;
+import org.springframework.stereotype.Controller;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+
+
+import java.util.List;
+
+@Controller
+public class CatGraphQLController {
+    private final CatService catService;
+
+    public CatGraphQLController(CatService catService) {
+        this.catService = catService;
+    }
+
+    @QueryMapping
+    public List<Cat> cats() {
+        return catService.getAllCats();
+    }
+}
