1585f03e4802fbcb72d9cab5a253e5cfde94c84d
alistanikzay <ali.kawsar.stanikzay@iths.se>
2025-11-07 01:40:45 +0100

    UserRepository & CatService

--- src/main/java/org/example/minikattkompis/service/CatService.java
+++ src/main/java/org/example/minikattkompis/service/CatService.java
@@ -2,7 +2,10 @@ package org.example.minikattkompis.service;
 
 import org.example.minikattkompis.model.Cat;
 import org.example.minikattkompis.repository.CatRepository;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
@@ -16,15 +19,21 @@ public class CatService {
         this.nameService = nameService;
     }
 
+    @Cacheable("cats")
     public List<Cat> getAllCats() {
+        System.out.println("Fetching cats from DB...");
         return repo.findAll();
     }
 
+    @CacheEvict(value = "cats", allEntries = true)
     public Cat addCat(Cat cat) {
         cat.setName(nameService.safeCatName(cat.getName()));
         return repo.save(cat);
     }
 
+    @Cacheable(value = "cats", key = "#id")
     public Cat getCat(Long id) {
-        return repo.findById(id).orElse(null); }
+        System.out.println("Fetching cat " + id + " from DB...");
+        return repo.findById(id).orElse(null);
     }
+}
