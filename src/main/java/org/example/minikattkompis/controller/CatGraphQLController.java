b3761859e32a013ad323f93745d9dd35d03ad4c2
alistanikzay <ali.kawsar.stanikzay@iths.se>
2025-11-07 23:29:42 +0100

    Changes by AhmadMudasirStanikzay

--- src/main/java/org/example/minikattkompis/controller/CatGraphQLController.java
+++ src/main/java/org/example/minikattkompis/controller/CatGraphQLController.java
@@ -2,22 +2,23 @@ package org.example.minikattkompis.controller;
 
 import org.example.minikattkompis.model.Cat;
 import org.example.minikattkompis.service.CatService;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-
 
 import java.util.List;
 
 @Controller
 public class CatGraphQLController {
+
     private final CatService catService;
 
-    public CatGraphQLController(CatService catService) {
+    public CatGraphQLController(@NonNull CatService catService) {
         this.catService = catService;
     }
 
     @QueryMapping
-    public List<Cat> cats() {
+    public @NonNull List<Cat> cats() {
         return catService.getAllCats();
     }
 }
